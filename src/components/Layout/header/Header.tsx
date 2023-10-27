2b80c8c18d0adc4c89988b7a048d0a1928e4d960
Oluwafolajimi Olaosebikan <oluwafolajimi.olaosebikan@cyber.cyberspace.net.ng>
2023-10-27 23:15:04 +0100

    wip: header screen conversion

--- src/components/Layout/header/Header.tsx
+++ src/components/Layout/header/Header.tsx
@@ -1,8 +1,24 @@
+import Button from "../../../custom/button/Button";
+import styles from '../header/header.module.scss';
+import { ReactComponent as Mic} from '../../../svg/mic.svg';
+import { ReactComponent as Search} from '../../../svg/icons.svg';
+
 const Header = () => {
     return (
-        <div>
-            <div>TichTrak</div>
-        </div>
+        <section className={styles.container}>
+            <div>
+                <div>
+                    <input placeholder="Search" />
+                    <Search className={styles.icon} />
+                </div>
+                
+                <button><Mic /></button>
+            </div>
+            <div>
+                <Button />
+            </div>
+            
+        </section>
     )
 }
 
