6d6a0764ce12a8f0cfa3ea1687f4a85f90236880
Oluwafolajimi Olaosebikan <oluwafolajimi.olaosebikan@cyber.cyberspace.net.ng>
2023-10-30 15:20:12 +0100

    wip: header design ongoing

--- src/components/Layout/header/Header.tsx
+++ src/components/Layout/header/Header.tsx
@@ -2,22 +2,29 @@ import Button from "../../../custom/button/Button";
 import styles from '../header/header.module.scss';
 import { ReactComponent as Mic} from '../../../svg/mic.svg';
 import { ReactComponent as Search} from '../../../svg/icons.svg';
+import { ReactComponent as Notification} from '../../../svg/Vector (8).svg';
 
 const Header = () => {
     return (
-        <section className={styles.container}>
-            <div>
+        <section>
+            <section className={styles.container}>
                 <div>
-                    <input placeholder="Search" />
-                    <Search className={styles.icon} />
+                    <div>
+                        <input placeholder="Search" />
+                        <Search className={styles.icon} />
+                    </div>
+                    <button><Mic /></button>
                 </div>
-                
-                <button><Mic /></button>
-            </div>
-            <div>
-                <Button />
-            </div>
-            
+                <div>
+                    <Notification />
+                    <div className={styles.line}></div>
+                    <Button />
+                </div>
+            </section>
+            <hr />
+            <section>
+
+            </section>
         </section>
     )
 }
